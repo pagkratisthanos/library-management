b1adca24e7e30643924e76280c7c66bb990cccef
pagkratisthanos <pagkratisthanos@gmail.com>
2026-03-17 15:34:14 +0200

    feat: add pagination queries to MemberRepository

--- src/main/java/com/library/management/repository/MemberRepository.java
+++ src/main/java/com/library/management/repository/MemberRepository.java
@@ -1,7 +1,13 @@
 package com.library.management.repository;
 
 import com.library.management.model.Member;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Page<Member> findAll(Pageable pageable);
+
+    Page<Member> findByDeletedFalse(Pageable pageable);
 }
