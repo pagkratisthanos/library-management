aaa733ffbdf68d1166adf10d052dbd77a54afe9d
pagkratisthanos <pagkratisthanos@gmail.com>
2026-03-17 16:38:41 +0200

    feat: add findBy and existsBy methods at MemberRepository

--- src/main/java/com/library/management/repository/MemberRepository.java
+++ src/main/java/com/library/management/repository/MemberRepository.java
@@ -5,9 +5,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     Page<Member> findAll(Pageable pageable);
 
     Page<Member> findByDeletedFalse(Pageable pageable);
+
+    Optional<Member> findByUuid(UUID uuid);
+
+    Optional<Member> findByUuidAndDeletedFalse(UUID uuid);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhoneNumber(String phoneNumber);
 }
