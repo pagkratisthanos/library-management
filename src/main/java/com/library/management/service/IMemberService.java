d0c81751cfde244eb821999db2bbfd21c7e846c7
pagkratisthanos <pagkratisthanos@gmail.com>
2026-03-25 21:46:03 +0200

    feat: add IMemberService interface

---
+++ src/main/java/com/library/management/service/IMemberService.java
@@ -0,0 +1,33 @@
+package com.library.management.service;
+
+import com.library.management.core.exceptions.EntityAlreadyExistsException;
+import com.library.management.core.exceptions.EntityInvalidArgumentException;
+import com.library.management.core.exceptions.EntityNotFoundException;
+import com.library.management.dto.MemberInsertDTO;
+import com.library.management.dto.MemberReadOnlyDTO;
+import com.library.management.dto.MemberUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.UUID;
+
+public interface IMemberService {
+
+    MemberReadOnlyDTO saveMember(MemberInsertDTO memberInsertDTO)
+        throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    MemberReadOnlyDTO updateMember(MemberUpdateDTO memberUpdateDTO)
+        throws EntityNotFoundException, EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    void deleteMemberByUuid(UUID uuid) throws EntityNotFoundException;
+
+    MemberReadOnlyDTO getMemberByUuid(UUID uuid) throws EntityNotFoundException;
+
+    MemberReadOnlyDTO getMemberByUUIDDeletedFalse(UUID uuid) throws EntityNotFoundException;
+
+    Page<MemberReadOnlyDTO> getMembersPaginated(Pageable pageable);
+
+    Page<MemberReadOnlyDTO> getMembersPaginatedAndDeletedFalse(Pageable pageable);
+
+    boolean isMemberExistByEmail(String email);
+}
