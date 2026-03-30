a9c1ba063f44847267ead8c965d8f8eeec48e49f
pagkratisthanos <pagkratisthanos@gmail.com>
2026-03-30 15:41:14 +0300

    feat: add existsByIsbn method at BookRepository

--- src/main/java/com/library/management/repository/BookRepository.java
+++ src/main/java/com/library/management/repository/BookRepository.java
@@ -1,7 +1,19 @@
 package com.library.management.repository;
 
 import com.library.management.model.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+
+    Optional<Book> findByUuid(UUID uuid);
+    Optional<Book> findByUuidAndDeletedFalse(UUID uuid);
+    Optional<Book> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
+    boolean existsByIsbnAndUuidNot(String isbn, UUID uuid);
+    Page<Book> findByDeletedFalse(Pageable pageable);
 }
