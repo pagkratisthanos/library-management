f3a4198c557a34ae6afea2b684d88cbd5b5f5617
pagkratisthanos <pagkratisthanos@gmail.com>
2026-03-30 15:39:03 +0300

    feat: add methods at AuthorRepositoryRepository

--- src/main/java/com/library/management/repository/AuthorRepository.java
+++ src/main/java/com/library/management/repository/AuthorRepository.java
@@ -1,7 +1,18 @@
 package com.library.management.repository;
 
 import com.library.management.model.Author;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface AuthorRepository extends JpaRepository<Author, Long> {
+
+    Optional<Author> findByUuid(UUID uuid);
+    Optional<Author> findByUuidAndDeletedFalse(UUID uuid);
+    Page<Author> findByDeletedFalse(Pageable pageable);
+    boolean existsByUuid(UUID uuid);
 }
+
