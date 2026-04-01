0cd660c8041b1f44a9500194fb59a70fe06a9b28
pagkratisthanos <pagkratisthanos@gmail.com>
2026-04-01 14:48:26 +0300

    feat: add IAuthorService at Service

---
+++ src/main/java/com/library/management/service/IAuthorService.java
@@ -0,0 +1,37 @@
+package com.library.management.service;
+
+import com.library.management.core.exceptions.EntityAlreadyExistsException;
+import com.library.management.core.exceptions.EntityInvalidArgumentException;
+import com.library.management.core.exceptions.EntityNotFoundException;
+import com.library.management.dto.AuthorInsertDTO;
+import com.library.management.dto.AuthorReadOnlyDTO;
+import com.library.management.dto.AuthorUpdateDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface IAuthorService {
+
+    AuthorReadOnlyDTO saveAuthor(AuthorInsertDTO authorInsertDTO)
+            throws EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    AuthorReadOnlyDTO updateAuthor(AuthorUpdateDTO authorUpdateDTO)
+            throws EntityNotFoundException, EntityAlreadyExistsException, EntityInvalidArgumentException;
+
+    void deleteAuthorByUuid(UUID uuid) throws EntityNotFoundException, EntityInvalidArgumentException;
+
+    AuthorReadOnlyDTO getAuthorByUuid(UUID uuid) throws EntityNotFoundException;
+
+    AuthorReadOnlyDTO getAuthorByUUIDDeletedFalse(UUID uuid) throws EntityNotFoundException;
+
+    Page<AuthorReadOnlyDTO> getAuthorsPaginated(Pageable pageable);
+
+    Page<AuthorReadOnlyDTO> getAuthorsPaginatedAndDeletedFalse(Pageable pageable);
+
+    boolean isAuthorExistByLastname(String lastname);
+
+    List<AuthorReadOnlyDTO> getAuthorsByBookUuid(UUID bookUuid) throws EntityNotFoundException;
+
+}
