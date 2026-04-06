c36dc48a6b0b75e2eafffe45f2c779e50bb94824
pagkratisthanos <pagkratisthanos@gmail.com>
2026-04-06 14:06:57 +0300

    feat: add custom query methods to RentalRepository

--- src/main/java/com/library/management/repository/RentalRepository.java
+++ src/main/java/com/library/management/repository/RentalRepository.java
@@ -1,7 +1,20 @@
 package com.library.management.repository;
 
 import com.library.management.model.Rental;
+import org.springframework.data.domain.Page;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface RentalRepository extends JpaRepository<Rental, Long> {
+
+        Optional<Rental> findByUuid(UUID uuid);
+        List<Rental> findByMember_Uuid(UUID memberUuid);
+        List<Rental> findByCopy_Uuid(UUID copyUuid);
+        Page<Rental> findByReturnDateIsNull(Pageable pageable);
+        boolean existsByUuid(UUID uuid);
 }
