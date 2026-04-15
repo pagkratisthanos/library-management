ea670667944f04441f88ccc993fc132ebeb02118
pagkratisthanos <pagkratisthanos@gmail.com>
2026-04-15 13:03:07 +0300

    fix: change condition type to CopyCondition

--- src/main/java/com/library/management/dto/CopyReadOnlyDTO.java
+++ src/main/java/com/library/management/dto/CopyReadOnlyDTO.java
@@ -1,9 +1,11 @@
 package com.library.management.dto;
 
+import com.library.management.model.CopyCondition;
+
 import java.util.UUID;
 
 public record CopyReadOnlyDTO(UUID uuid, UUID bookUuid,
                               String bookTitle, Boolean available,
-                              String condition
+                              CopyCondition condition
                               ) {
 }
