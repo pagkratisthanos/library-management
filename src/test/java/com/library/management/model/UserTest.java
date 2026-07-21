9bbb2298af1663822bba29397dd38a055e312ebe
pagkratisthanos <pagkratisthanos@gmail.com>
2026-07-21 12:54:11 +0300

    test: add UserTest with 10 test cases

---
+++ src/test/java/com/library/management/model/UserTest.java
@@ -0,0 +1,94 @@
+package com.library.management.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserTest {
+
+    private User user;
+    private Role role;
+    private Capability capability;
+
+    @BeforeEach
+    void setUp() {
+        capability = new Capability();
+        capability.setName("VIEW_AUTHOR");
+
+        role = new Role();
+        role.setName("ADMIN");
+        role.addCapability(capability);
+
+        user = new User();
+        user.setUsername("admin");
+        user.setPassword("password");
+        role.addUser(user);
+    }
+
+    @Test
+    void getAuthorities_shouldContainRoleAuthority() {
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+        assertThat(authorities)
+                .extracting(GrantedAuthority::getAuthority)
+                .contains("ROLE_ADMIN");
+    }
+
+    @Test
+    void getAuthorities_shouldContainCapabilityAuthority() {
+        Collection<? extends GrantedAuthority> authorities = user.getAuthorities();
+        assertThat(authorities)
+                .extracting(GrantedAuthority::getAuthority)
+                .contains("VIEW_AUTHOR");
+    }
+
+    @Test
+    void isAccountNonExpired_shouldReturnTrue() {
+        assertThat(user.isAccountNonExpired()).isTrue();
+    }
+
+    @Test
+    void isAccountNonLocked_shouldReturnTrue() {
+        assertThat(user.isAccountNonLocked()).isTrue();
+    }
+
+    @Test
+    void isCredentialsNonExpired_shouldReturnTrue() {
+        assertThat(user.isCredentialsNonExpired()).isTrue();
+    }
+
+    @Test
+    void isEnabled_whenNotDeleted_shouldReturnTrue() {
+        assertThat(user.isEnabled()).isTrue();
+    }
+
+    @Test
+    void isEnabled_whenDeleted_shouldReturnFalse() {
+        user.softDelete();
+        assertThat(user.isEnabled()).isFalse();
+    }
+
+    @Test
+    void softDelete_shouldMarkUserAsDeleted() {
+        user.softDelete();
+        assertThat(user.isDeleted()).isTrue();
+        assertThat(user.getDeletedAt()).isNotNull();
+    }
+
+    @Test
+    void equals_whenSameUsername_shouldReturnTrue() {
+        User anotherUser = new User();
+        anotherUser.setUsername("admin");
+        assertThat(user).isEqualTo(anotherUser);
+    }
+
+    @Test
+    void equals_whenDifferentUsername_shouldReturnFalse() {
+        User anotherUser = new User();
+        anotherUser.setUsername("librarian");
+        assertThat(user).isNotEqualTo(anotherUser);
+    }
+}
