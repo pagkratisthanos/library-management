60e34fc12cda33d15b1673cd37d86987e4e42d0a
pagkratisthanos <pagkratisthanos@gmail.com>
2026-07-25 18:53:43 +0300

    feat: add RoleRestController with GET /api/roles endpoint

---
+++ src/main/java/com/library/management/api/RoleRestController.java
@@ -0,0 +1,34 @@
+package com.library.management.api;
+
+import com.library.management.dto.RoleReadOnlyDTO;
+import com.library.management.mapper.RoleMapper;
+import com.library.management.model.Role;
+import com.library.management.service.IRoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/roles")
+@RequiredArgsConstructor
+public class RoleRestController {
+
+    private final IRoleService roleService;
+    private final RoleMapper roleMapper;
+
+    @Operation(summary = "Get all roles")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @GetMapping
+    public ResponseEntity<List<RoleReadOnlyDTO>> getAllRoles() {
+        List<Role> roles = roleService.getAllRoles();
+        return ResponseEntity.ok(roles.stream()
+                .map(roleMapper::mapToRoleReadOnlyDTO)
+                .toList());
+    }
+}
